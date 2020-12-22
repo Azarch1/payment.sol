a7db2c4cfa151fac9fbe617e258f91c1ff45bda8
SkynetDev <andrejohnson0017@gmail.com>
2020-12-21 21:05:55 -0500

    Added designated credits from the smart contract

--- payment.sol
+++ payment.sol
@@ -4,15 +4,25 @@ contract recieveMoneyContract {
     
     uint public amountRecieved;
     
+    // Function to send ether to the smart contract
+    
     function recieveMoney() public payable {
         amountRecieved += msg.value;
     }
     
+    // function to discern the current value of the smart contract
     function getBalance() public view returns(uint) {
       return address(this).balance;
     }
     
-    function SendMoney() public {
-        
+    // vulnerable function where anyone with an account can withdraw ether from the contract
+    function sendMoney() public {
+    address payable to = msg.sender;
+    to.transfer(this.getBalance());
+    }
+
+    // this function takes an argument that designates the specific address
+    function sendMoney2(address payable _to) public {
+        _to.transfer(this.getBalance());
     }
 }
